0a6f12c9bb1b46c915df4cf183700e103e0747a6
Federico E. Bonilla <federicoebonilla@gmail.com>
2026-04-10 13:10:53 -0300

    RegisterForm con estillos

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
-  return <>
-    <h1>Aqui estara el landing inicial.</h1>
-  </>;
+  return (
+    <>
+      <h1>Aqui estara el landing inicial.</h1>
+    </>
+  );
 }
