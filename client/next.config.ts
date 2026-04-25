7a86ec303243f11ecf4b46ae1c6da79792bbd9f1
Hiram Carreño <143430604+Abiff11@users.noreply.github.com>
2026-04-25 14:37:10 -0600

    feat/Crud-logoutOwner

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
   turbopack: {
     root: path.join(__dirname),
   },
